546be9d8ffdd286d04d73812070aedfbe335180d
PM Villa <pierremichel.villa@rakuten.com>
2020-12-16 13:50:27 +0900

    refactored secret script

--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 use_frameworks!
 platform :ios, '10.0'
 
+secrets = ["RIAM_CONFIG_URL", "RIAM_APP_SUBSCRIPTION_KEY"]
+
 target 'RInAppMessaging_Example' do
   pod 'RInAppMessaging', :path => '.'
   pod 'SwiftLint', '~> 0.40.0'
@@ -13,5 +15,5 @@ target 'RInAppMessaging_Example' do
 end
 
 post_install do |installer|
-  system("./configure-secrets.sh")
+  system("./configure-secrets.sh InAppMessaging #{secrets.join(" ")}")
 end
--- configure-secrets.sh
+++ configure-secrets.sh
@@ -1,5 +1,29 @@
 #!/bin/sh -e
 
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
+NOBOLD='\033[21m'
+
+usage () {
+cat <<HELP_USAGE
+usage: $0 param1 param[n]...
+
+   Parameters:
+   - param1: The SDK name which needs to have a -Secrets.xcconfig file
+   - param[n]: a list of secrets environment variables to put in secret config file
+
+  Example usage:
+    $0 MiniApp RMA_API_ENDPOINT RAS_PROJECT_SUBSCRIPTION_KEY RAS_PROJECT_IDENTIFIER
+HELP_USAGE
+}
+
+if [ $# == 0 ]; then
+    usage
+    exit 1
+fi
+
 #In order to have the // in https://, we need to split it with an empty variable substitution via $()
 secureDoubleSlashForXCConfig() {
 	ENDPOINT=$1
@@ -9,26 +33,10 @@ secureDoubleSlashForXCConfig() {
 	SECURE_DOUBLE_SLASH_FOR_XCCONFIG_RESULT="${ENDPOINT/$URL_DOUBLE_SLASH_TO_BE_REPLACED/$BY_2_SLASHES_SPLITTED_BY_EMPTY_VARIABLE}" 
 }
 
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-SECRETS_FILE=../InAppMessaging-Secrets.xcconfig
+vars=("${@:2}")
+SECRETS_FILE=../${1}-Secrets.xcconfig
 
-echo "Configuring project's build secrets in $SECRETS_FILE..."
-
-#
-# New secrets should be added below (following the same format)
-#
-declare -a vars=(RIAM_CONFIG_URL RIAM_APP_SUBSCRIPTION_KEY)
-for var_name in "${vars[@]}"
-do
-  if [ -z "$(eval "echo \$$var_name")" ]; then
-    echo "${RED}ERROR:${NOCOLOR} Before building the project you must set environment variable $var_name. See project README for instructions."
-  fi
-done
-
-secureDoubleSlashForXCConfig ${RIAM_CONFIG_URL:=https://www.example.com}
-CONFIG_URL=$SECURE_DOUBLE_SLASH_FOR_XCCONFIG_RESULT
-SUBSCRIPTION_KEY=${RIAM_APP_SUBSCRIPTION_KEY}
+echo "${GREEN}Configuring project's build secrets in ${BOLD}$SECRETS_FILE${GREEN}...${NOCOLOR}"
 
 # Overwrite secrets xcconfig and add file header
 echo "// Secrets configuration for the app." > $SECRETS_FILE
@@ -40,8 +48,36 @@ echo "//" >> $SECRETS_FILE
 echo "// Add new secrets configuration in ./configure-secrets.sh" >> $SECRETS_FILE
 echo "//" >> $SECRETS_FILE
 echo "// In order to have the // in https://, we need to split it with an empty" >> $SECRETS_FILE
-echo "// variable substitution via \$() e.g. ROOT_URL = https:/\$()/www.endpoint.com" >> $SECRETS_FILE
+echo "// variable substitution via \$() e.g. ROOT_URL = https:/\$()/www.example.com" >> $SECRETS_FILE
 
-# Set secrets from environment variables
-echo "RIAM_CONFIG_URL = $CONFIG_URL" >> $SECRETS_FILE
-echo "RIAM_APP_SUBSCRIPTION_KEY = $SUBSCRIPTION_KEY" >> $SECRETS_FILE
+longest_name=-1
+for var_name in ${vars[@]}
+do
+	current=${#var_name}
+   if [ $current -gt $longest_name ]
+   then
+      longest_name=$current
+   fi
+done
+
+success=true
+for var_name in ${vars[@]}
+do
+  nbstars=$(($longest_name - ${#var_name} + 5))
+  stars=$(printf '%*s' $nbstars '')	
+  value=$(eval "echo \$$var_name")
+  if [ -z "$value" ]; then
+    >&2 echo "➜ ${BOLD}$var_name${NOCOLOR} ${stars// /*} ${RED}ERROR!${NOCOLOR} Missing environment variable ${BOLD}$var_name${NOCOLOR}."
+    success=false
+  else
+  	echo "➜ ${BOLD}$var_name${NOCOLOR} ${stars// /*} ${GREEN}OK${NOCOLOR}"
+  fi
+  secureDoubleSlashForXCConfig $value
+  # Set secrets from environment variables
+  cmd='echo "${var_name} = $SECURE_DOUBLE_SLASH_FOR_XCCONFIG_RESULT"'
+  eval ${cmd} >> $SECRETS_FILE
+done
+
+if [ $success = false ] ; then
+	echo "Before building the project you must set missing environment variables. See project README for instructions."
+fi
