d7df4ead2361b6ba9ac29323dd2e7b68a98bf1d6
Aaron Lee <aaron@onefatgiraffe.com>
2021-08-06 13:49:01 +0900

    Revert "fix: integral image geometry SDKCF-4022 (#104)"
    This reverts commit d52766aa1f945c124f3831e83ea5b97a462aa0d4.

--- RInAppMessaging/Classes/Extensions/CGSize+IAM.swift
+++
@@ -1,6 +0,0 @@
-extension CGSize {
-    /// returns a `CGSize` with the smallest integer values for its size to prevent drawing on pixel boundaries
-    var integral: CGSize {
-        CGSize(width: ceil(width), height: ceil(height))
-    }
-}
