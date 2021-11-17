5e62b1914a5ef68908132b85fd932f6e7419bef6
Aaron Lee <aaron@onefatgiraffe.com>
2021-11-17 15:35:51 +0900

    WIP

--- Tests/Tests/UIColorExtensionSpec.swift
+++ Tests/Tests/UIColorExtensionSpec.swift
@@ -7,12 +7,22 @@ class UIColorExtensionsSpec: QuickSpec {
     override func spec() {
         describe("UIColor+IAM") {
             context("when calling brightness methods") {
-                it("expect black to objectively be of 0 brightness") {
+                it("expect sRGB black to objectively be of 0 brightness") {
+                    expect(UIColor.blackRGB.brightness) == 0
+                    expect(UIColor.blackRGB.isBright).to(beFalse())
+                }
+
+                it("expect sRGB white to objectively be of 1 brightness") {
+                    expect(UIColor.whiteRGB.brightness) == 1
+                    expect(UIColor.whiteRGB.isBright).to(beTrue())
+                }
+
+                it("expect ext greyscale colour spaced black to objectively be of 0 brightness") {
                     expect(UIColor.black.brightness) == 0
                     expect(UIColor.black.isBright).to(beFalse())
                 }
 
-                it("expect white to objectively be of 1 brightness") {
+                it("expect ext greyscale colour spaced white to objectively be of 1 brightness") {
                     expect(UIColor.white.brightness) == 1
                     expect(UIColor.white.isBright).to(beTrue())
                 }
