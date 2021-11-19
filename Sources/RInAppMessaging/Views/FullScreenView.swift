d9dde978c85493af684f01e6579595a37276d453
Aaron Lee <aaron@onefatgiraffe.com>
2021-11-19 15:36:07 +0900

    WIP

--- Sources/RInAppMessaging/Views/FullScreenView.swift
+++ Sources/RInAppMessaging/Views/FullScreenView.swift
@@ -7,6 +7,15 @@ internal class FullScreenView: FullView {
         return "IAMView-FullScreen"
     }
 
+    private lazy var statusBarBackgroundView: UIView = {
+         let backgroundView = UIView()
+         backgroundView.translatesAutoresizingMaskIntoConstraints = false
+         backgroundView.backgroundColor = .statusBarOverlayColor
+         return backgroundView
+     }()
+     private lazy var statusBarBackgroundViewHeightConstraint = statusBarBackgroundView.heightAnchor
+         .constraint(equalToConstant: UIApplication.shared.statusBarFrame.height)
+
     override var mode: Mode {
         return .fullScreen
     }
@@ -18,4 +27,24 @@ internal class FullScreenView: FullView {
         uiConstants.dialogViewWidthOffset = 0
         uiConstants.dialogViewWidthMultiplier = 1.0
     }
+
+    override func setup(viewModel: FullViewModel) {
+         super.setup(viewModel: viewModel)
+
+         addSubview(statusBarBackgroundView)
+         NSLayoutConstraint.activate([
+             statusBarBackgroundView.topAnchor.constraint(equalTo: topAnchor),
+             statusBarBackgroundView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+             statusBarBackgroundView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+             statusBarBackgroundViewHeightConstraint
+         ])
+     }
+
+     override func layoutSubviews() {
+         super.layoutSubviews()
+
+         statusBarBackgroundView.backgroundColor = .statusBarOverlayColor
+         // statusBarFrame.height is 0 when status bar is hidden
+         statusBarBackgroundViewHeightConstraint.constant = UIApplication.shared.statusBarFrame.height
+     }
 }
