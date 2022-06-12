5e21911c288b7f9899af8012ffc4269a5fefc8e6
Michał Kalinowski <imaginaris@users.noreply.github.com>
2022-06-13 01:08:05 +0200

    build: Bump Quick to v5.0 (fix for Xcode 13.3) (SDKCF-5124)

--- Podfile
+++ Podfile
@@ -1,6 +1,8 @@
 use_frameworks!
 platform :ios, '12.0'
 
+# Variable necessary to parse Shock podspec file from their master branch
+ENV['LIB_VERSION'] = '6.1.1'
 secrets = ["RIAM_CONFIG_URL", "RIAM_APP_SUBSCRIPTION_KEY"]
 
 target 'RInAppMessaging_Example' do
@@ -9,16 +11,18 @@ target 'RInAppMessaging_Example' do
   pod 'RSDKUtils', '~> 3.0.0', :testspecs => ['Nimble', 'TestHelpers']
 
   abstract_target 'Tests-Common' do
-    pod 'Quick'
+    pod 'Quick', '~> 5.0'
     pod 'Nimble'
 
     target 'Tests'
 
     target 'UITests' do
-      # Version 6.1 currently relies on swift-nio version (2.38) which has following issue
+      # The currently released Shock version relies on swift-nio version (2.38) which has following issue:
       # https://github.com/apple/swift-nio/issues/2073
-      # Version 6.0 uses older swift-nio version which doesn't have that problem
-      pod 'Shock', '~> 6.0.0'
+      # The issue has been fixed in SwiftNIO version 2.40
+      # Until a new version of Shock is released, the pod declaration should point to master branch which contains updated SwiftNIO dependency
+      # (Older versions of Shock cannot be used with Xcode 13.3)'
+      pod 'Shock', :git => 'https://github.com/justeat/Shock'
     end
 
     target 'IntegrationTests'
--- Tests/Tests/MainContainerSpec.swift
+++ Tests/Tests/MainContainerSpec.swift
@@ -22,24 +22,25 @@ class MainContainerSpec: QuickSpec {
             it("will have all dependencies resolved") {
                 let instances: [Any?] = [
                     dependencyManager.resolve(type: CommonUtility.self),
-                    dependencyManager.resolve(type: ConfigurationServiceType.self),
+                    dependencyManager.resolve(type: ReachabilityType.self),
                     dependencyManager.resolve(type: ConfigurationRepositoryType.self),
                     dependencyManager.resolve(type: ConfigurationManagerType.self),
+                    dependencyManager.resolve(type: UserDataCacheable.self),
                     dependencyManager.resolve(type: CampaignRepositoryType.self),
                     dependencyManager.resolve(type: EventMatcherType.self),
-                    dependencyManager.resolve(type: CampaignDispatcherType.self),
                     dependencyManager.resolve(type: AccountRepositoryType.self),
+                    dependencyManager.resolve(type: ConfigurationServiceType.self),
                     dependencyManager.resolve(type: DisplayPermissionServiceType.self),
                     dependencyManager.resolve(type: RouterType.self),
+                    dependencyManager.resolve(type: Randomizer.self),
+                    dependencyManager.resolve(type: CampaignDispatcherType.self),
                     dependencyManager.resolve(type: MessageMixerServiceType.self),
                     dependencyManager.resolve(type: ImpressionServiceType.self),
+                    dependencyManager.resolve(type: CampaignsListManagerType.self),
                     dependencyManager.resolve(type: CampaignsValidatorType.self),
-                    dependencyManager.resolve(type: ReachabilityType.self),
                     dependencyManager.resolve(type: FullViewPresenterType.self),
                     dependencyManager.resolve(type: SlideUpViewPresenterType.self),
-                    dependencyManager.resolve(type: CampaignsListManagerType.self),
-                    dependencyManager.resolve(type: CampaignTriggerAgentType.self),
-                    dependencyManager.resolve(type: UserDataCacheable.self)
+                    dependencyManager.resolve(type: CampaignTriggerAgentType.self)
                 ]
                 expect(instances).to(allPass({ $0 != nil }))
                 // this test will fail if there are any cycle references
