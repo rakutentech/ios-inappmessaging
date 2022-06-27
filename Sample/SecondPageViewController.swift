52540f9b54b4fc2e8a2269cccfe9323cff636279
Aaron Lee <aaron@onefatgiraffe.com>
2022-06-27 15:41:38 +0900

    Mock

--- Sample/SecondPageViewController.swift
+++ Sample/SecondPageViewController.swift
@@ -1,8 +1,10 @@
 import UIKit
+import OSLog
 
 class SecondPageViewController: UIViewController {
 
     @IBAction func returnToHomepage(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+        os_log("second page 1")
     }
 }
