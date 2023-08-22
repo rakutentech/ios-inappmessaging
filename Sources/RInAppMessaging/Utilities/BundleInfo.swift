1c8c4189a96aef9bd649d3f79d3b2e1c9c87c7d7
Soumen Rautray <soumen.rautray@rakuten.com>
2023-08-22 15:08:49 +0530

    chore: refactor code (SDKCF-6709)

--- Sources/RInAppMessaging/Utilities/BundleInfo.swift
+++ Sources/RInAppMessaging/Utilities/BundleInfo.swift
@@ -68,11 +68,9 @@ internal extension Bundle {
     }
     
     static var rmcSdkVersion: String? {
-        if let rmcBundle = Bundle(identifier: "RMC-RMC-resources") {
-            if let path = rmcBundle.path(forResource: "RMC/Resources/Version", ofType: "plist") {
+        if let path = rmcResources?.path(forResource: "RMC/Resources/Version", ofType: "plist") {
                 let versionDict = NSDictionary(contentsOfFile: path)
                 return versionDict?["rmcSdkVersion"] as? String
-            }
         }
         return nil
     }
