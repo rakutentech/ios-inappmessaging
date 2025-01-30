463ad368547fc688499394bda6574301ed6baf25
Soumen Rautray <soumen.rautray@rakuten.com>
2025-01-30 15:51:07 +0530

    chore: Change carousel cell color to campaign background color (RMCCX-8178)

--- Sources/RInAppMessaging/Views/CarouselView.swift
+++ Sources/RInAppMessaging/Views/CarouselView.swift
@@ -10,6 +10,7 @@ import UIKit
     private var currentIndex = 0
     private var hasReachedLastImage = false
     private var campaignMode: Mode = .none
+    private var carouselBgColor: UIColor = .clear
     var presenter: FullViewPresenterType?
 
     required init?(coder: NSCoder) {
@@ -24,10 +25,11 @@ import UIKit
         stopAutoScroll()
     }
 
-    func configure(carouselData: [CarouselData], presenter: FullViewPresenterType, campaignMode: Mode) {
+    func configure(carouselData: [CarouselData], presenter: FullViewPresenterType, campaignMode: Mode, backgroundColor: UIColor) {
         self.carouselData = carouselData
         self.presenter = presenter
         self.campaignMode = campaignMode
+        self.carouselBgColor = backgroundColor
         setupCollectionView()
         setupPageControl()
         startAutoScroll()
@@ -79,8 +81,11 @@ extension CarouselView: UICollectionViewDataSource, UICollectionViewDelegateFlow
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CarouselCell.identifier, for: indexPath) as? CarouselCell else {
             return UICollectionViewCell()
         }
+
+        collectionView.backgroundColor = (carouselData[indexPath.item].image != nil) ? carouselBgColor : .clear
         cell.configure(with: carouselData[indexPath.item].image,
-                       altText: carouselData[indexPath.item].altText)
+                       altText: carouselData[indexPath.item].altText,
+                       cellBgColor: carouselBgColor)
         return cell
     }
 
