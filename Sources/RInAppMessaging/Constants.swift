b12d77ca5f3ab42283d245354eca0507a4a7f148
Soumen Rautray <soumen.rautray@rakuten.com>
2025-07-07 10:27:33 +0530

    chore: changes for fetching eventlogger changes to demoApp (RMCCX-8722)

--- Sources/RInAppMessaging/Constants.swift
+++ Sources/RInAppMessaging/Constants.swift
@@ -49,7 +49,7 @@ internal enum Constants {
     }
 
     enum Versions {
-        static let sdkVersion = "9.2.0-snapshot"
+        static let sdkVersion = "9.2.0"
     }
 
     enum RAnalytics: String {
